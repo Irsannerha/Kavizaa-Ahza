15b2212c8ddeed2f670bac438b8b877fc1f663c3
Irsan Romardi Harahap <44488538+Irsannerha@users.noreply.github.com>
2023-06-27 10:52:18 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Kavizaahza",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "Poerty Blog",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "kavizaahza.dev@gmail.com",
+    linkedin: "kavizaahza",
+    github: "kavizaahza",
+    instagram: "kavizaahza",
   },
   projects: [
     {
